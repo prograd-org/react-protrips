5a0f6de64f43444139e8757a9aed97cc7fac8731
Aaryan Shaikh <aayanshaikh27638@gmail.com>
2021-10-11 18:59:53 +0530

    5522

--- protrips/src/components/Display.jsx
+++ protrips/src/components/Display.jsx
@@ -1,16 +1,73 @@
 import React, { Component } from 'react'
 
 export default class Display extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            trips: [...this.props.trips],
+            type: "All"
+        }
+    }
+    typeAll = () => {
+        this.setState({
+            trips: [...this.props.trips],
+            type: "All"
+        })
+    }
+    typeTrek = () => {
+        this.setState({
+            trips: [...this.props.trips.filter((trip) => {
+                return trip.type === "trek"
+            })],
+            type: "Trek"
+        })
+    }
+    typeClub = () => {
+        this.setState({
+            trips: [...this.props.trips.filter((trip) => {
+                return trip.type === "club"
+            })],
+            type: "Club"
+        })
+    }
+    typeTropic = () => {
+        this.setState({
+            trips: [...this.props.trips.filter((trip) => {
+                return trip.type === "tropic"
+            })],
+            type: "Tropic"
+        })
+    }
     render() {
         return (
-            <div>
-                <p className="headD">All Trips</p>
+            <>
+                <p className="headD">{this.state.type} Trips</p>
                 <table>
                     <tbody>
-                        <tr></tr>
+                        <tr>
+                            <th>Date</th>
+                            <th>Place</th>
+                            <th>Type</th>
+                        </tr>
+                        {
+                            this.state.trips.map((trip) => {
+                                return <tr>
+                                    <td>{trip.date}</td>
+                                    <td>{trip.place}</td>
+                                    <td>{trip.type}</td>
+                                </tr>
+                            })
+                        }
                     </tbody>
                 </table>
-            </div>
+                <div className="filter">
+                    Filter by:
+                    <span onClick={this.typeAll}>All</span><span className="symbol">&#9898;</span>
+                    <span onClick={this.typeTrek}>Treks</span><span className="symbol">&#9898;</span>
+                    <span onClick={this.typeClub}>Clubs</span><span className="symbol">&#9898;</span>
+                    <span onClick={this.typeTropic}>Tropics</span>
+                </div>
+            </>
         )
     }
 }
--- protrips/src/components/Home.jsx
+++ protrips/src/components/Home.jsx
@@ -3,14 +3,20 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSun, faMountain, faBeer } from '@fortawesome/free-solid-svg-icons'
 
 export default class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            trips: [...this.props.trips]
+        }
+    }
     render() {
         return (
             <>
-                <p className="home"><span className="total">4</span>trips</p>
+                <p className="home"><span className="total">{this.state.trips.length}</span>trips</p>
                 <div className="trips">
-                    <span>1<FontAwesomeIcon icon={faSun} /></span>
-                    <span>2<FontAwesomeIcon icon={faMountain} /></span>
-                    <span>1<FontAwesomeIcon icon={faBeer} /></span>
+                    <span>{this.state.trips.filter((trip) => { return trip.type === "tropic" }).length}<FontAwesomeIcon icon={faSun} /></span>
+                    <span>{this.state.trips.filter((trip) => { return trip.type === "trek" }).length}<FontAwesomeIcon icon={faMountain} /></span>
+                    <span>{this.state.trips.filter((trip) => { return trip.type === "club" }).length}<FontAwesomeIcon icon={faBeer} /></span>
                 </div>
             </>
         )
